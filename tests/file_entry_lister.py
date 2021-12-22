dd8afb5e37bdf17042c52c0f89dabacd4297bade
Joachim Metz <joachim.metz@gmail.com>
2021-12-22 12:08:47 +0100

    Added support for bodyfile output #2

--- tests/file_entry_lister.py
+++ tests/file_entry_lister.py
@@ -46,6 +46,82 @@ class FileEntryListerTest(test_lib.BaseTestCase):
     paths = [path for path, _ in file_entries]
     self.assertEqual(paths, expected_paths)
 
+  def testGetBodyfileModeString(self):
+    """Tests the _GetBodyfileModeString function."""
+    test_lister = file_entry_lister.FileEntryLister()
+
+    mode_string = test_lister._GetBodyfileModeString(0)
+    self.assertEqual(mode_string, '----------')
+
+    mode_string = test_lister._GetBodyfileModeString(0o777)
+    self.assertEqual(mode_string, '-rwxrwxrwx')
+
+    mode_string = test_lister._GetBodyfileModeString(0x1000)
+    self.assertEqual(mode_string, 'p---------')
+
+    mode_string = test_lister._GetBodyfileModeString(0x2000)
+    self.assertEqual(mode_string, 'c---------')
+
+    mode_string = test_lister._GetBodyfileModeString(0x4000)
+    self.assertEqual(mode_string, 'd---------')
+
+    mode_string = test_lister._GetBodyfileModeString(0x6000)
+    self.assertEqual(mode_string, 'b---------')
+
+    mode_string = test_lister._GetBodyfileModeString(0xa000)
+    self.assertEqual(mode_string, 'l---------')
+
+    mode_string = test_lister._GetBodyfileModeString(0xc000)
+    self.assertEqual(mode_string, 's---------')
+
+  def testGetBasePathSpecs(self):
+    """Tests the GetBasePathSpecs function."""
+    path = self._GetTestFilePath(['image.qcow2'])
+    self._SkipIfPathNotExists(path)
+
+    test_lister = file_entry_lister.FileEntryLister()
+
+    expected_path_spec = path_spec_factory.Factory.NewPathSpec(
+        dfvfs_definitions.TYPE_INDICATOR_OS, location=path)
+    expected_path_spec = path_spec_factory.Factory.NewPathSpec(
+        dfvfs_definitions.TYPE_INDICATOR_QCOW, parent=expected_path_spec)
+    expected_path_spec = path_spec_factory.Factory.NewPathSpec(
+        dfvfs_definitions.PREFERRED_EXT_BACK_END, location='/',
+        parent=expected_path_spec)
+
+    base_path_specs = test_lister.GetBasePathSpecs(path)
+    self.assertEqual(base_path_specs, [expected_path_spec])
+
+  def testGetBodyfileEntry(self):
+    """Tests the GetBodyfileEntry function."""
+    path = self._GetTestFilePath(['image.qcow2'])
+    self._SkipIfPathNotExists(path)
+
+    test_lister = file_entry_lister.FileEntryLister()
+
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        dfvfs_definitions.TYPE_INDICATOR_OS, location=path)
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        dfvfs_definitions.TYPE_INDICATOR_QCOW, parent=path_spec)
+    path_spec = path_spec_factory.Factory.NewPathSpec(
+        dfvfs_definitions.TYPE_INDICATOR_TSK, location='/passwords.txt',
+        parent=path_spec)
+
+    file_system = resolver.Resolver.OpenFileSystem(path_spec)
+    file_entry = resolver.Resolver.OpenFileEntry(path_spec)
+
+    file_entries = list(test_lister._ListFileEntry(
+        file_system, file_entry, ['']))
+
+    self.assertEqual(len(file_entries), 1)
+
+    expected_bodyfile_entry = (
+        '0|/passwords.txt|15|-r--------|151107|5000|116|1337961653|'
+        '1337961653|1337961663|')
+
+    bodyfile_entry = test_lister.GetBodyfileEntry(*file_entries[0])
+    self.assertEqual(bodyfile_entry, expected_bodyfile_entry)
+
   def testListFileEntries(self):
     """Tests the ListFileEntries function."""
     path = self._GetTestFilePath(['image.qcow2'])
@@ -72,24 +148,6 @@ class FileEntryListerTest(test_lib.BaseTestCase):
 
     self.assertEqual(len(file_entries), len(expected_paths))
     self.assertEqual(paths, expected_paths)
-
-  def testGetBasePathSpecs(self):
-    """Tests the GetBasePathSpecs function."""
-    path = self._GetTestFilePath(['image.qcow2'])
-    self._SkipIfPathNotExists(path)
-
-    test_lister = file_entry_lister.FileEntryLister()
-
-    expected_path_spec = path_spec_factory.Factory.NewPathSpec(
-        dfvfs_definitions.TYPE_INDICATOR_OS, location=path)
-    expected_path_spec = path_spec_factory.Factory.NewPathSpec(
-        dfvfs_definitions.TYPE_INDICATOR_QCOW, parent=expected_path_spec)
-    expected_path_spec = path_spec_factory.Factory.NewPathSpec(
-        dfvfs_definitions.PREFERRED_EXT_BACK_END, location='/',
-        parent=expected_path_spec)
-
-    base_path_specs = test_lister.GetBasePathSpecs(path)
-    self.assertEqual(base_path_specs, [expected_path_spec])
 
 
 if __name__ == '__main__':
