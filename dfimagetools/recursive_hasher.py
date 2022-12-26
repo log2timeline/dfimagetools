8a66168babc25756f57aa13d8c2b8b57235720a6
Joachim Metz <joachim.metz@gmail.com>
2022-12-26 09:11:33 +0100

    Worked on recursive hasher script

--- dfimagetools/recursive_hasher.py
+++ dfimagetools/recursive_hasher.py
@@ -6,13 +6,8 @@ import hashlib
 import logging
 import re
 
-from dfvfs.helpers import volume_scanner
-from dfvfs.lib import definitions as dfvfs_definitions
-from dfvfs.lib import errors as dfvfs_errors
-from dfvfs.resolver import resolver
 
-
-class RecursiveHasher(volume_scanner.VolumeScanner):
+class RecursiveHasher(object):
   """Recursively calculates message digest hashes of data streams."""
 
   # Class constant that defines the default read buffer size.
@@ -75,47 +70,10 @@ class RecursiveHasher(volume_scanner.VolumeScanner):
 
     return hash_context.hexdigest()
 
-  def _CalculateHashesFileEntry(
-      self, file_system, file_entry, parent_path_segments, output_writer):
-    """Recursive calculates hashes starting with the file entry.
-
-    Args:
-      file_system (dfvfs.FileSystem): file system.
-      file_entry (dfvfs.FileEntry): file entry.
-      parent_path_segments (str): path segments of the full path of the parent
-          file entry.
-      output_writer (StdoutWriter): output writer.
-    """
-    path_segments = parent_path_segments + [file_entry.name]
-    lookup_path = tuple(path_segments[1:])
-
-    for data_stream in file_entry.data_streams:
-      hash_value = None
-      if (lookup_path, data_stream.name) not in self._PATHS_TO_IGNORE:
-        hash_value = self._CalculateHashDataStream(file_entry, data_stream.name)
-
-      display_path = self._GetDisplayPath(
-          file_entry.path_spec, path_segments, data_stream.name)
-      output_writer.WriteFileHash(display_path, hash_value or 'N/A')
-
-    try:
-      for sub_file_entry in file_entry.sub_file_entries:
-        self._CalculateHashesFileEntry(
-            file_system, sub_file_entry, path_segments, output_writer)
-
-    except (IOError, dfvfs_errors.AccessError,
-            dfvfs_errors.BackEndError) as exception:
-      path_specification_string = self._GetPathSpecificationString(
-          file_entry.path_spec)
-      logging.warning((
-          'Unable to open path specification:\n{0:s}'
-          'with error: {1!s}').format(path_specification_string, exception))
-
-  def _GetDisplayPath(self, path_spec, path_segments, data_stream_name):
+  def _GetDisplayPath(self, path_segments, data_stream_name):
     """Retrieves a path to display.
 
     Args:
-      path_spec (dfvfs.PathSpec): path specification of the file entry.
       path_segments (list[str]): path segments of the full path of the file
           entry.
       data_stream_name (str): name of the data stream.
@@ -124,12 +82,6 @@ class RecursiveHasher(volume_scanner.VolumeScanner):
       str: path to display.
     """
     display_path = ''
-
-    if path_spec.HasParent():
-      parent_path_spec = path_spec.parent
-      if parent_path_spec and parent_path_spec.type_indicator in (
-          dfvfs_definitions.PARTITION_TABLE_TYPE_INDICATORS):
-        display_path = ''.join([display_path, parent_path_spec.location])
 
     path_segments = [
         segment.translate(self._ESCAPE_CHARACTERS) for segment in path_segments]
@@ -160,21 +112,22 @@ class RecursiveHasher(volume_scanner.VolumeScanner):
 
     return path_spec_string
 
-  def CalculateHashes(self, base_path_specs, output_writer):
-    """Recursive calculates hashes starting with the base path specification.
+  def CalculateHashesFileEntry(self, file_entry, path_segments):
+    """Recursive calculates hashes starting with the file entry.
 
     Args:
-      base_path_specs (list[dfvfs.PathSpec]): source path specification.
-      output_writer (StdoutWriter): output writer.
-    """
-    for base_path_spec in base_path_specs:
-      file_system = resolver.Resolver.OpenFileSystem(base_path_spec)
-      file_entry = resolver.Resolver.OpenFileEntry(base_path_spec)
-      if file_entry is None:
-        path_specification_string = self._GetPathSpecificationString(
-            base_path_spec)
-        logging.warning('Unable to open base path specification:\n{0:s}'.format(
-            path_specification_string))
-        continue
+      file_entry (dfvfs.FileEntry): file entry.
+      path_segments (str): path segments of the full path of file entry.
 
-      self._CalculateHashesFileEntry(file_system, file_entry, [], output_writer)
+    Yields:
+      tuple[str, str]: display path and hash value.
+    """
+    lookup_path = tuple(path_segments[1:])
+
+    for data_stream in file_entry.data_streams:
+      hash_value = None
+      if (lookup_path, data_stream.name) not in self._PATHS_TO_IGNORE:
+        hash_value = self._CalculateHashDataStream(file_entry, data_stream.name)
+
+      display_path = self._GetDisplayPath(path_segments, data_stream.name)
+      yield display_path, hash_value
--- tools/recursive_hasher.py
+++ tools/recursive_hasher.py
@@ -2,130 +2,17 @@
 # -*- coding: utf-8 -*-
 """Script to recursively calculate a message digest hash of data streams."""
 
-import abc
 import argparse
 import logging
 import sys
 
+from dfvfs.helpers import command_line as dfvfs_command_line
+from dfvfs.helpers import volume_scanner as dfvfs_volume_scanner
 from dfvfs.lib import errors as dfvfs_errors
-from dfvfs.helpers import command_line
-from dfvfs.helpers import volume_scanner
 
+from dfimagetools import file_entry_lister
 from dfimagetools import helpers
 from dfimagetools import recursive_hasher
-
-
-class OutputWriter(object):
-  """Output writer interface."""
-
-  def __init__(self, encoding='utf-8'):
-    """Initializes an output writer.
-
-    Args:
-      encoding (Optional[str]): input encoding.
-    """
-    super(OutputWriter, self).__init__()
-    self._encoding = encoding
-    self._errors = 'strict'
-
-  def _EncodeString(self, string):
-    """Encodes the string.
-
-    Args:
-      string (str): string to encode.
-
-    Returns:
-      bytes: encoded string.
-    """
-    try:
-      # Note that encode() will first convert string into a Unicode string
-      # if necessary.
-      encoded_string = string.encode(self._encoding, errors=self._errors)
-    except UnicodeEncodeError:
-      if self._errors == 'strict':
-        logging.error(
-            'Unable to properly write output due to encoding error. '
-            'Switching to error tolerant encoding which can result in '
-            'non Basic Latin (C0) characters to be replaced with "?" or '
-            '"\\ufffd".')
-        self._errors = 'replace'
-
-      encoded_string = string.encode(self._encoding, errors=self._errors)
-
-    return encoded_string
-
-  @abc.abstractmethod
-  def Close(self):
-    """Closes the output writer object."""
-
-  @abc.abstractmethod
-  def Open(self):
-    """Opens the output writer object."""
-
-  @abc.abstractmethod
-  def WriteFileHash(self, path, hash_value):
-    """Writes the file path and hash.
-
-    Args:
-      path (str): path of the file.
-      hash_value (str): message digest hash calculated over the file data.
-    """
-
-
-class FileOutputWriter(OutputWriter):
-  """Output writer that writes to a file."""
-
-  def __init__(self, path, encoding='utf-8'):
-    """Initializes an output writer.
-
-    Args:
-      path (str): name of the path.
-      encoding (Optional[str]): input encoding.
-    """
-    super(FileOutputWriter, self).__init__(encoding=encoding)
-    self._file_object = None
-    self._path = path
-
-  def Close(self):
-    """Closes the output writer object."""
-    self._file_object.close()
-
-  def Open(self):
-    """Opens the output writer object."""
-    # Using binary mode to make sure to write Unix end of lines, so we can
-    # compare output files cross-platform.
-    self._file_object = open(self._path, 'wb')  # pylint: disable=consider-using-with
-
-  def WriteFileHash(self, path, hash_value):
-    """Writes the file path and hash to file.
-
-    Args:
-      path (str): path of the file.
-      hash_value (str): message digest hash calculated over the file data.
-    """
-    string = '{0:s}\t{1:s}\n'.format(hash_value, path)
-
-    encoded_string = self._EncodeString(string)
-    self._file_object.write(encoded_string)
-
-
-class StdoutWriter(OutputWriter):
-  """Output writer that writes to stdout."""
-
-  def Close(self):
-    """Closes the output writer object."""
-
-  def Open(self):
-    """Opens the output writer object."""
-
-  def WriteFileHash(self, path, hash_value):
-    """Writes the file path and hash to stdout.
-
-    Args:
-      path (str): path of the file.
-      hash_value (str): message digest hash calculated over the file data.
-    """
-    print('{0:s}\t{1:s}'.format(hash_value, path))
 
 
 def Main():
@@ -138,14 +25,10 @@ def Main():
       'Calculates a message digest hash for every file in a directory or '
       'storage media image.'))
 
+  # TODO: add source group
   argument_parser.add_argument(
       '--back_end', '--back-end', dest='back_end', action='store',
       metavar='NTFS', default=None, help='preferred dfVFS back-end.')
-
-  argument_parser.add_argument(
-      '--output_file', '--output-file', dest='output_file', action='store',
-      metavar='source.hashes', default=None, help=(
-          'path of the output file, default is to output to stdout.'))
 
   argument_parser.add_argument(
       '--partitions', '--partition', dest='partitions', action='store',
@@ -176,7 +59,7 @@ def Main():
 
   argument_parser.add_argument(
       'source', nargs='?', action='store', metavar='image.raw',
-      default=None, help='path of the directory or storage media image.')
+      default=None, help='path of the storage media image.')
 
   options = argument_parser.parse_args()
 
@@ -192,23 +75,9 @@ def Main():
   logging.basicConfig(
       level=logging.INFO, format='[%(levelname)s] %(message)s')
 
-  if options.output_file:
-    output_writer = FileOutputWriter(options.output_file)
-  else:
-    output_writer = StdoutWriter()
+  mediator = dfvfs_command_line.CLIVolumeScannerMediator()
 
-  try:
-    output_writer.Open()
-  except IOError as exception:
-    print('Unable to open output writer with error: {0!s}.'.format(
-        exception))
-    print('')
-    return False
-
-  mediator = command_line.CLIVolumeScannerMediator()
-  hasher = recursive_hasher.RecursiveHasher(mediator=mediator)
-
-  volume_scanner_options = volume_scanner.VolumeScannerOptions()
+  volume_scanner_options = dfvfs_volume_scanner.VolumeScannerOptions()
   volume_scanner_options.partitions = mediator.ParseVolumeIdentifiersString(
       options.partitions)
 
@@ -221,36 +90,36 @@ def Main():
   volume_scanner_options.volumes = mediator.ParseVolumeIdentifiersString(
       options.volumes)
 
-  return_value = True
+  entry_lister = file_entry_lister.FileEntryLister(mediator=mediator)
 
   try:
-    base_path_specs = hasher.GetBasePathSpecs(
+    base_path_specs = entry_lister.GetBasePathSpecs(
         options.source, options=volume_scanner_options)
     if not base_path_specs:
       print('No supported file system found in source.')
       print('')
       return False
 
-    hasher.CalculateHashes(base_path_specs, output_writer)
+    hasher = recursive_hasher.RecursiveHasher()
+    for base_path_spec in base_path_specs:
+      file_entries_generator = entry_lister.ListFileEntries([base_path_spec])
 
-    print('')
-    print('Completed.')
+      for file_entry, path_segments in file_entries_generator:
+        for display_path, hash_value in hasher.CalculateHashesFileEntry(
+            file_entry, path_segments):
+          print('{0:s}\t{1:s}'.format(hash_value or 'N/A', display_path))
 
   except dfvfs_errors.ScannerError as exception:
-    return_value = False
-
+    print(f'[ERROR] {exception!s}', file=sys.stderr)
     print('')
-    print('[ERROR] {0!s}'.format(exception))
+    return False
 
-  except dfvfs_errors.UserAbort as exception:
-    return_value = False
-
+  except KeyboardInterrupt:
+    print('Aborted by user.', file=sys.stderr)
     print('')
-    print('Aborted.')
+    return False
 
-  output_writer.Close()
-
-  return return_value
+  return True
 
 
 if __name__ == '__main__':
