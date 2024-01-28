879ef901a435886fb573d45137075ed1b96cb3e1
Joachim Metz <joachim.metz@gmail.com>
2024-01-28 07:21:46 +0100

    Changed tools to entry points

--- dfimagetools/recursive_hasher.py
+++ dfimagetools/recursive_hasher.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Script to recursively calculate a message digest hash of data streams."""
+"""Helper to recursively calculate a message digest hash of data streams."""
 
-import argparse
 import hashlib
 import logging
-import sys
-
-from dfvfs.lib import errors as dfvfs_errors
 
 from dfimagetools import definitions
-from dfimagetools import file_entry_lister
-from dfimagetools.helpers import command_line
 
 
 class RecursiveHasher(object):
@@ -114,71 +108,3 @@ class RecursiveHasher(object):
 
       display_path = self._GetDisplayPath(path_segments, data_stream.name)
       yield display_path, hash_value
-
-
-if __name__ == '__main__':
-  argument_parser = argparse.ArgumentParser(description=(
-      'Calculates a message digest hash for every file in a directory or '
-      'storage media image.'))
-
-  # TODO: add output group
-  argument_parser.add_argument(
-      '--no_aliases', '--no-aliases', dest='use_aliases', action='store_false',
-      default=True, help=(
-          'Disable the use of partition and/or volume aliases such as '
-          '/apfs{f449e580-e355-4e74-8880-05e46e4e3b1e} and use indices '
-          'such as /apfs1 instead.'))
-
-  # TODO: add source group
-  command_line.AddStorageMediaImageCLIArguments(argument_parser)
-
-  argument_parser.add_argument(
-      'source', nargs='?', action='store', metavar='image.raw',
-      default=None, help='path of the storage media image.')
-
-  options = argument_parser.parse_args()
-
-  if not options.source:
-    print('Source value is missing.')
-    print('')
-    argument_parser.print_help()
-    print('')
-    sys.exit(1)
-
-  logging.basicConfig(
-      level=logging.INFO, format='[%(levelname)s] %(message)s')
-
-  mediator, volume_scanner_options = (
-      command_line.ParseStorageMediaImageCLIArguments(options))
-
-  entry_lister = file_entry_lister.FileEntryLister(
-      mediator=mediator, use_aliases=options.use_aliases)
-
-  try:
-    base_path_specs = entry_lister.GetBasePathSpecs(
-        options.source, options=volume_scanner_options)
-    if not base_path_specs:
-      print('No supported file system found in source.')
-      print('')
-      sys.exit(1)
-
-    hasher = RecursiveHasher()
-    for base_path_spec in base_path_specs:
-      file_entries_generator = entry_lister.ListFileEntries([base_path_spec])
-
-      for file_entry, path_segments in file_entries_generator:
-        for display_path, hash_value in hasher.CalculateHashesFileEntry(
-            file_entry, path_segments):
-          print('{0:s}\t{1:s}'.format(hash_value or 'N/A', display_path))
-
-  except dfvfs_errors.ScannerError as exception:
-    print(f'[ERROR] {exception!s}', file=sys.stderr)
-    print('')
-    sys.exit(1)
-
-  except KeyboardInterrupt:
-    print('Aborted by user.', file=sys.stderr)
-    print('')
-    sys.exit(1)
-
-  sys.exit(0)
