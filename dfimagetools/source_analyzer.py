f0a655c581ea8df865e69075ddccf07b43946632
Joachim Metz <joachim.metz@gmail.com>
2025-11-24 13:22:23 +0100

    Changes for release and deployment

--- dfimagetools/source_analyzer.py
+++ dfimagetools/source_analyzer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Helper to recursively check for volumes and file systems."""
 
--- tests/path_filters.py
+++ tests/path_filters.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Tests for the helper for filtering based on a path."""
 
